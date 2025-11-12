9885682b4b98decefc2dbc5ca339c83a439503cd
Saad <saadullahkh55@gmail.com>
2025-11-12 18:24:53 +0500

    Add pulse effect to cart icon on item removal: enhance visual feedback when items are removed from the cart

--- cart.js
+++ cart.js
@@ -128,6 +128,7 @@ function attachCartListeners() {
         removeBtn.addEventListener("click", () => {
             selectedItems.splice(index, 1);
             setCartItems(selectedItems);
+            pulseCartIcon();
             updateCartTotal();
             totalAmountOfItems();
         });
@@ -208,13 +209,13 @@ function promoCodeDiscount() {
 }
 
 function pulseCartIcon() {
-  const ping = document.getElementById("cart-ping");
-  let dynamicCart = JSON.parse(localStorage.getItem("addedToCart")) || [];
-if (dynamicCart.length > 0) {
-    ping.style.display = "block"; // show pulse if cart has items
-  } else {
-    ping.style.display = "none"; // hide if cart empty
-  }
+    const ping = document.getElementById("cart-ping");
+    let dynamicCart = JSON.parse(localStorage.getItem("addedToCart")) || [];
+    if (dynamicCart.length > 0) {
+        ping.style.display = "block"; // show pulse if cart has items
+    } else {
+        ping.style.display = "none"; // hide if cart empty
+    }
 }
 
 
